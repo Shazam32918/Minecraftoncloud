6f6f2cd92dc8754784e3e92f30a18e639565fea3
Shazam32918 <gomezgonzalezandressimonei@gmail.com>
2025-04-11 17:42:59 -0400

    Update inicio.sh

--- inicio.sh
+++ inicio.sh
@@ -1,25 +1,3 @@
-pip install colorama
-from colorama import Fore, Style
-
-def print_big_text():
-    text = [
-        "  N   N  EEEEE   OOO   K   K  ",
-        "  NN  N  E      O   O  K  K   ",
-        "  N N N  EEEE   O   O  KKK    ",
-        "  N  NN  E      O   O  K  K   ",
-        "  N   N  EEEEE   OOO   K   K  ",
-    ]
-
-    colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.CYAN]
-
-    for i, line in enumerate(text):
-        print(colors[i % len(colors)] + line)
-
-    print(Style.RESET_ALL)  # Resetear estilos al final
-
-if __name__ == "__main__":
-    print_big_text()
-    
 mkdir servidor
 cd servidor
 wget https://api.papermc.io/v2/projects/paper/versions/1.21.4/builds/224/downloads/paper-1.21.4-224.jar
